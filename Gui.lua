67397fbf47c41dcac5bfb67ab0a0c2fa7f7a4fc1
Ultra34 <103150652+Ultra34@users.noreply.github.com>
2022-09-23 10:58:53 -0500

    The Script Hub

--- Gui.lua
+++ Gui.lua
@@ -1,10 +1,26 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Nathan's Script Hub", "BloodTheme")
 
--- MAIN
-local Main = Window:NewTab("Main")
+-- Unnamed-ESP
+local Main = Window:NewTab("Unnamed-ESP")
 local MainSection = Main:NewSection("Unnamed-ESP")
 
-MainSection:NewButton("inject", "Injects The Script", function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))()
+MainSection:NewButton("Inject", "Injects The Script", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua"))()
+end)
+
+-- Aimbot
+local Main = Window:NewTab("Aimbot")
+local MainSection = Main:NewSection("Aimbot")
+
+MainSection:NewButton("Inject", "Injects The Script", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/UniversalAimbot.lua"))()
+end)
+
+-- V.G Hub
+local Main = Window:NewTab("V.G Hub")
+local MainSection = Main:NewSection("V.G Hub")
+
+MainSection:NewButton("Inject", "Injects The Script", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
 end)
